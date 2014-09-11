6eb979e672aa73aa570b621ff5bbe1d917967f23
Zarino Zappia <mail@zarino.co.uk>
2014-09-11 23:45:12 +0100

    pull in json location data, plot on map

--- js/main.js
+++ js/main.js
@@ -6,4 +6,11 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(window.map);
 
-// L.marker([53.405050, -2.990580]).addTo(window.map);
+$.getJSON('https://gist.githubusercontent.com/paulfurley/723698b43127ddf9fe1c/raw/0778350edf1d2774476205d3a865b9ba560b15d2/citybike.json', function(apiResponse){
+  $.each(apiResponse.locations, function(){
+    var m = L.marker([this.latitude, this.longitude]);
+    m.bindPopup(this.locationName, {closeButton: false});
+    m.addTo(window.map);
+  });
+});
+
