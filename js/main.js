330faa20416c2c9f0cc768f59e8d77e81494c56a
Zarino Zappia <mail@zarino.co.uk>
2014-09-16 22:24:48 +0100

    toolbar controls to switch between viewing available bikes and available locks

--- js/main.js
+++ js/main.js
@@ -30,22 +30,24 @@ var generatePopupHTML = function(locationName, availableBikes, availableLocks){
   return $wrapper.html();
 }
 
-window.map = L.map('map').setView([53.404973, -2.979250], 15);
+window.map = L.map('map').setView([53.404973, -2.979250], 14);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(window.map);
 
+window.markers = L.layerGroup().addTo(map);
+
 $.getJSON('https://gist.githubusercontent.com/paulfurley/723698b43127ddf9fe1c/raw/0778350edf1d2774476205d3a865b9ba560b15d2/citybike.json', function(apiResponse){
   $.each(apiResponse.locations, function(){
-    console.log(this);
-    var m = L.marker([this.latitude, this.longitude], {
+    var marker = L.marker([this.latitude, this.longitude], {
       icon: new NumberedIcon({ number: this.availableBikes })
     });
-    m.bindPopup(generatePopupHTML(this.locationName, this.availableBikes, this.availableLocks), {
+    marker.data = this;
+    marker.bindPopup(generatePopupHTML(this.locationName, this.availableBikes, this.availableLocks), {
       closeButton: false
     });
-    m.addTo(window.map);
+    window.markers.addLayer(marker);
   });
 });
 
@@ -91,3 +93,20 @@ var showCurrentLocation = function(e) {
 }
 
 window.map.on('locationfound', showCurrentLocation);
+
+var switchMarkerNumbers = function(){
+  if($('#show-bikes').is('.active')){
+    window.markers.eachLayer(function(marker){
+      marker.setIcon(new NumberedIcon({ number: marker.data.availableBikes }))
+    })
+  } else {
+    window.markers.eachLayer(function(marker){
+      marker.setIcon(new NumberedIcon({ number: marker.data.availableLocks }))
+    })
+  }
+}
+
+$('#toolbar a').on('click', function(){
+  $('#toolbar .active').removeClass('active').parent().siblings().children('a').addClass('active');
+  switchMarkerNumbers();
+});
