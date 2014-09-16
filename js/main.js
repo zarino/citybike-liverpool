3e72e3ba1eda4302e0590e4525d0b336f03707c2
Zarino Zappia <mail@zarino.co.uk>
2014-09-16 21:29:07 +0100

    numbered markers

--- js/main.js
+++ js/main.js
@@ -1,5 +1,35 @@
 L.Icon.Default.imagePath = '/img/';
 
+var NumberedIcon = L.Icon.extend({
+  options: {
+    iconUrl: L.Icon.Default.imagePath + 'marker-icon.png',
+    iconRetinaUrl: L.Icon.Default.imagePath + 'marker-icon-2x.png',
+    shadowUrl: L.Icon.Default.imagePath + 'marker-shadow.png',
+    shadowRetinaUrl: L.Icon.Default.imagePath + 'marker-shadow-2x.png',
+    iconSize: [30, 40],
+    iconAnchor: [15, 40],
+    popupAnchor: [1, -34],
+    shadowAnchor: [12, 32],
+    shadowSize: [35, 35],
+    number: ''
+  },
+  createIcon: function(){
+    var $div = $('<div>');
+    $('<img>').attr('src', this.options['iconUrl']).appendTo($div);
+    $('<div>').addClass('number').text(this.options['number']).appendTo($div);
+    this._setIconStyles($div[0], 'icon');
+    return $div[0];
+  }
+});
+
+var generatePopupHTML = function(locationName, availableBikes, availableLocks){
+  var $wrapper = $('<div>');
+  $('<h2>').text(locationName).appendTo($wrapper);
+  $('<p>').text('' + availableBikes + ' bike' + (availableBikes == 1 ? '' : 's') + ' available').appendTo($wrapper);
+  $('<p>').text('' + availableLocks + ' empty lock' + (availableLocks == 1 ? '' : 's')).appendTo($wrapper);
+  return $wrapper.html();
+}
+
 window.map = L.map('map').setView([53.404973, -2.979250], 15);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
@@ -8,8 +38,13 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 
 $.getJSON('https://gist.githubusercontent.com/paulfurley/723698b43127ddf9fe1c/raw/0778350edf1d2774476205d3a865b9ba560b15d2/citybike.json', function(apiResponse){
   $.each(apiResponse.locations, function(){
-    var m = L.marker([this.latitude, this.longitude]);
-    m.bindPopup(this.locationName, {closeButton: false});
+    console.log(this);
+    var m = L.marker([this.latitude, this.longitude], {
+      icon: new NumberedIcon({ number: this.availableBikes })
+    });
+    m.bindPopup(generatePopupHTML(this.locationName, this.availableBikes, this.availableLocks), {
+      closeButton: false
+    });
     m.addTo(window.map);
   });
 });
